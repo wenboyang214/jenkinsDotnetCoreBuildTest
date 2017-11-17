da0ed4f2bb581f7d2bc9cb0fe32f9bc504196832
Wenbo Yang <v-wenbya@microsoft.com>
2017-11-17 10:45:20 +0800

    add the project

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<20;i++)
+            {
+                Console.WriteLine($"Hello World! -----{i}");
+            }
+            Console.ReadLine();
+            
+        }
+    }
+}
